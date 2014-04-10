4f9d90e477570efb6f3f6954713ce2bd9323d0be
RyanDur <ryan.p.durling@gmail.com>
2014-04-10 11:27:42 +0100

    refactor question view test

--- set-up/src/test/java/gui/pij/ryan/durling/views/QuestionViewTest.java
+++ set-up/src/test/java/gui/pij/ryan/durling/views/QuestionViewTest.java
@@ -5,36 +5,46 @@ import org.junit.Test;
 import org.loadui.testfx.GuiTest;
 import pij.ryan.durling.controllers.QuizCreator;
 import pij.ryan.durling.exceptions.IllegalQuizCreationException;
-import pij.ryan.durling.views.pages.AnswerView;
+import pij.ryan.durling.views.pages.QuestionView;
 import pij.ryan.durling.views.pages.QuestionViewImpl;
 
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 public class QuestionViewTest extends GuiTest {
-    private String addQuestion = "#add-question-button";
-    private String addQuestionArea = "#add-question";
-    private String scoreField  = "#score";
     private QuizCreator quizCreator;
-    private AnswerView answerView;
+    private QuestionView questionView;
 
     @Override
     protected Parent getRootNode() {
         quizCreator = mock(QuizCreator.class);
-        answerView = mock(AnswerView.class);
-        return new QuestionViewImpl();
+        questionView = new QuestionViewImpl();
+        return (Parent) questionView;
     }
 
     @Test
     public void should() throws IllegalQuizCreationException {
+        questionView.getAddQuestionButton().setOnAction(e -> {
+            try {
+                quizCreator.addQuestion(anyString(), anyInt());
+            } catch (IllegalQuizCreationException e1) {
+                e1.printStackTrace();
+            }
+        });
+
         String bacon = "Bacon";
         String text = "9";
+        String addQuestion = "#add-question-button";
+        String addQuestionArea = "#add-question";
+        String scoreField = "#score";
         click(addQuestionArea)
                 .type(bacon)
                 .click(scoreField)
                 .type(text)
                 .click(addQuestion);
 
-        verify(quizCreator).addQuestion(bacon, Integer.parseInt(text));
+        verify(quizCreator).addQuestion(anyString(), anyInt());
     }
 }
