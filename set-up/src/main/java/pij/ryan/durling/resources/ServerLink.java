de5fbed96ded0d3256b397a4aeaf065030136680
RyanDur <ryan.p.durling@gmail.com>
2014-04-12 18:59:02 +0100

    link to server

--- set-up/src/main/java/pij/ryan/durling/resources/ServerLink.java
+++ set-up/src/main/java/pij/ryan/durling/resources/ServerLink.java
@@ -1,5 +1,8 @@
 package pij.ryan.durling.resources;
 
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
 public interface ServerLink {
-    Server getServer();
+    Server getServer() throws RemoteException, NotBoundException;
 }
--- set-up/src/main/java/pij/ryan/durling/resources/ServerLinkImpl.java
+++ set-up/src/main/java/pij/ryan/durling/resources/ServerLinkImpl.java
@@ -1,8 +1,20 @@
 package pij.ryan.durling.resources;
 
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class ServerLinkImpl implements ServerLink {
+
+    private final Registry registry;
+
+    public ServerLinkImpl() throws RemoteException {
+        registry = LocateRegistry.getRegistry("localhost", 1099);
+    }
+
     @Override
-    public Server getServer() {
-        return null;
+    public Server getServer() throws RemoteException, NotBoundException {
+        return (Server) registry.lookup("Server");
     }
 }
