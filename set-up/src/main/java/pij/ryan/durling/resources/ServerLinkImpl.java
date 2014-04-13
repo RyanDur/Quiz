b1d0252e93eb981ac18dbd24e56a53d411d06dd0
RyanDur <ryan.p.durling@gmail.com>
2014-04-13 09:37:32 +0100

    slf4j log4j addition

--- set-up/src/main/java/pij/ryan/durling/resources/ServerLinkImpl.java
+++ set-up/src/main/java/pij/ryan/durling/resources/ServerLinkImpl.java
@@ -1,5 +1,8 @@
 package pij.ryan.durling.resources;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
@@ -7,14 +10,25 @@ import java.rmi.registry.Registry;
 
 public class ServerLinkImpl implements ServerLink {
 
-    private final Registry registry;
+    private static final Logger log = LoggerFactory.getLogger(ServerLinkImpl.class);
+    private Registry registry;
 
-    public ServerLinkImpl() throws RemoteException {
-        registry = LocateRegistry.getRegistry("localhost", 1099);
+    public ServerLinkImpl()  {
+        try {
+            registry = LocateRegistry.getRegistry("localhost", 1099);
+        } catch (RemoteException e) {
+            log.info("Server is not running");
+        }
     }
 
     @Override
-    public Server getServer() throws RemoteException, NotBoundException {
-        return (Server) registry.lookup("Server");
+    public Server getServer()  {
+        Server server = null;
+        try {
+            server = (Server) registry.lookup("Server");
+        } catch (RemoteException | NotBoundException e) {
+            log.info("Server is not running");
+        }
+        return server;
     }
 }
--- set-up/src/main/java/pij/ryan/durling/views/Start.java
+++ set-up/src/main/java/pij/ryan/durling/views/Start.java
@@ -4,13 +4,16 @@ package pij.ryan.durling.views;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import javafx.application.Application;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.apache.log4j.BasicConfigurator;
 import pij.ryan.durling.modules.EditorModule;
 import pij.ryan.durling.views.pages.Editor;
 import pij.ryan.durling.views.pages.EditorImpl;
 
 public class Start extends Application {
+
 
     public static void main(String[] args) {
         launch(args);
@@ -18,10 +21,10 @@ public class Start extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        BasicConfigurator.configure();
         Injector injector = Guice.createInjector(new EditorModule());
         Editor editor = injector.getInstance(EditorImpl.class);
-
-        stage.setScene((Scene) editor);
+        stage.setScene(new Scene((Parent) editor, 625, 500));
         stage.show();
     }
 }
