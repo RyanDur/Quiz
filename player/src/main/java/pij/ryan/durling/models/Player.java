97497b2e81c6f4e475d5186fd61707c2e2fa3fb3
RyanDur <ryan.p.durling@gmail.com>
2014-04-13 21:44:49 +0100

    add Player

--- player/src/main/java/pij/ryan/durling/models/Player.java
+++ player/src/main/java/pij/ryan/durling/models/Player.java
@@ -1,11 +1,5 @@
 package pij.ryan.durling.models;
 
-import pij.ryan.durling.models.Quiz;
-
-import java.util.List;
-
 public interface Player {
-    List<Quiz> getQuizList();
 
-    Quiz getQuiz(int quizIndex);
 }
--- player/src/main/java/pij/ryan/durling/resources/QuizServer.java
+++ player/src/main/java/pij/ryan/durling/resources/QuizServer.java
@@ -1,4 +1,9 @@
 package pij.ryan.durling.resources;
 
+import pij.ryan.durling.models.Quiz;
+
+import java.util.List;
+
 public interface QuizServer {
+    List<Quiz> getQuizList();
 }
