265a7ceb22d30363f42a29750be839d4b65fcf51
RyanDur <ryan.p.durling@gmail.com>
2014-04-17 23:23:27 +0100

    quiz implementation

--- server/src/main/java/pij/ryan/durling/models/QuizImpl.java
+++ server/src/main/java/pij/ryan/durling/models/QuizImpl.java
@@ -1,14 +1,17 @@
 package pij.ryan.durling.models;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class QuizImpl implements Quiz {
     private String title;
     private int id;
+    private final Set<Question> questions;
 
     public QuizImpl(String title, int id) {
         this.title = title;
         this.id = id;
+        questions = new HashSet<>();
     }
 
     @Override
@@ -23,16 +26,16 @@ public class QuizImpl implements Quiz {
 
     @Override
     public boolean valid() {
-        return false;
+        return !questions.isEmpty();
     }
 
     @Override
     public void add(Question question) {
-
+        questions.add(question);
     }
 
     @Override
     public Set<Question> getQuestions() {
-        return null;
+        return questions;
     }
 }
