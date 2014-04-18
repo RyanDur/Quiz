e88b5438600ab1f8434cb1068a22eae384b5ba54
RyanDur <ryan.p.durling@gmail.com>
2014-04-18 23:57:19 +0100

    remove spike

--- server/src/main/java/example/Subtraction.java
+++
@@ -1,8 +0,0 @@
-package example;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface Subtraction extends Remote {
-    int subtract(int a, int b) throws RemoteException;
-}
